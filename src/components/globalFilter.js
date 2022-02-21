efca420090f8f1578321b0bb643864bef77a8ad5
unknown <aswath@accuknox.com>
2022-02-22 02:38:07 +0530

    added filter for table

--- src/components/globalFilter.js
+++ src/components/globalFilter.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GlobalFilter = ({ filter, setFilter }) => {
     return (
         <span>
-            <input className='w-50 m-2' placeholder='search by Name / designation / team' type="text" value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
+            <input className='w-100 m-2' placeholder='search by Name / designation / team' type="text" value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
         </span>
     )
 }
