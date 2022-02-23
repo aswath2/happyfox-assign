06b80bb562193b163fccc3353afbaec79f0e5d09
Aswath <67890637+aswath2@users.noreply.github.com>
2022-02-23 23:41:36 +0530

    prettier (#5)

--- src/components/filterTable.js
+++ src/components/filterTable.js
@@ -1,73 +1,89 @@
-import React from 'react'
+import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import BTable from 'react-bootstrap/Table';
+import BTable from "react-bootstrap/Table";
 
-import { useTable, useGlobalFilter } from 'react-table';
-import { GlobalFilter } from './globalFilter';
-import { Dropdown } from 'react-bootstrap';
+import { useTable, useGlobalFilter } from "react-table";
+import { GlobalFilter } from "./globalFilter";
+import { Dropdown } from "react-bootstrap";
 
 function Table({ columns, data, setSelectedDropDownValue }) {
+  const options = ["All", "sales", "developer", "accounts", "ceo"];
 
-    const options = ["All","sales", "developer", "accounts", "ceo"]
+  const {
+    getTableProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter
+  );
 
-    const { getTableProps, headerGroups, rows, prepareRow, state, setGlobalFilter } = useTable({
-        columns,
-        data,
-    }, useGlobalFilter)
+  const { globalFilter } = state;
 
-    const { globalFilter } = state
-
-    return (
-        <>
-            <div className="d-flex position-relative">
-                <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-                <div className="position-absolute end-0">
-                    <Dropdown>
-                        filter:{''}
-                        <Dropdown.Toggle style={{backgroundColor: '#FFFFFF', borderColor:'#f2f2f2', color: '#000'}} className='ms-2' variant="success" id="dropdown-basic">
-                            By Team
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                            {options.map((items) => (
-                                <Dropdown.Item onClick={() => setSelectedDropDownValue(items)} >{items}</Dropdown.Item>
-                            ))}
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
-            </div>
-            <BTable striped bordered hover size="sm" {...getTableProps()}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}>
-                                    {column.render('Header')}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody>
-                    {rows.map((row, i) => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return (
-                                        <td {...cell.getCellProps()}>
-                                            {cell.render('Cell')}
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </BTable>
-        </>
-    )
+  return (
+    <>
+      <div className="d-flex position-relative">
+        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+        <div className="position-absolute end-0">
+          <Dropdown>
+            filter:{""}
+            <Dropdown.Toggle
+              style={{
+                backgroundColor: "#FFFFFF",
+                borderColor: "#f2f2f2",
+                color: "#000",
+              }}
+              className="ms-2"
+              variant="success"
+              id="dropdown-basic"
+            >
+              By Team
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {options.map((items) => (
+                <Dropdown.Item onClick={() => setSelectedDropDownValue(items)}>
+                  {items}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </div>
+      <BTable striped bordered hover size="sm" {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </BTable>
+    </>
+  );
 }
 
-export default Table
+export default Table;
--- src/server.js
+++ src/server.js
@@ -2,148 +2,216 @@
 import { createServer } from "miragejs";
 
 let graphData = {
-    "id": 1,
-    "name": "Mark Hill",
-    "designation": "Chief executive officer",
-    "children": [],
-    "hChildren": [
+  id: 1,
+  name: "Mark Hill",
+  designation: "Chief executive officer",
+  children: [],
+  hChildren: [
+    {
+      id: 2,
+      name: "Joe Linux",
+      designation: "Chief Technology",
+      manager: 1,
+      children: [
         {
-            "id": 2,
-            "name": "Joe Linux",
-            "designation": "Chief Technology",
-            "manager": 1,
-            "children": [
-                {
-                    "id": 5,
-                    "name": "Ron Blomquist",
-                    "designation": "Chief Information",
-                    "manager": 2,
-                },
-                {
-                    "id": 6,
-                    "name": "Michael Rubin",
-                    "designation": "Chief Innovation officer",
-                    "manager": 2,
-                }
-            ]
+          id: 5,
+          name: "Ron Blomquist",
+          designation: "Chief Information",
+          manager: 2,
         },
         {
-            "id": 3,
-            "name": "Linda May",
-            "designation": "Chief Business Officer",
-            "manager": 1,
-            "children": [
-                {
-                    "id": 7,
-                    "name": "Alice Lopex",
-                    "designation": "Chief Communication officer",
-                    "manager": 3,
-                },
-                {
-                    "id": 8,
-                    "name": "Mary Johnson",
-                    "designation": "Chief Brand officer",
-                    "manager": 3,
-                },
-                {
-                    "id": 9,
-                    "name": "Krik Douglas",
-                    "designation": "Chief Buisness",
-                    "manager": 3,
-                }
-            ]
+          id: 6,
+          name: "Michael Rubin",
+          designation: "Chief Innovation officer",
+          manager: 2,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Linda May",
+      designation: "Chief Business Officer",
+      manager: 1,
+      children: [
+        {
+          id: 7,
+          name: "Alice Lopex",
+          designation: "Chief Communication officer",
+          manager: 3,
         },
         {
-            "id": 4,
-            "name": "John Green",
-            "designation": "Chief Accounting officer",
-            "manager": 1,
-            "children": [
-                {
-                    "id": 10,
-                    "name": "Erica Reel",
-                    "designation": "Chief Customer Officer",
-                    "manager": 4,
-                }
-            ]
-        }
-    ]
-}
+          id: 8,
+          name: "Mary Johnson",
+          designation: "Chief Brand officer",
+          manager: 3,
+        },
+        {
+          id: 9,
+          name: "Krik Douglas",
+          designation: "Chief Buisness",
+          manager: 3,
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "John Green",
+      designation: "Chief Accounting officer",
+      manager: 1,
+      children: [
+        {
+          id: 10,
+          name: "Erica Reel",
+          designation: "Chief Customer Officer",
+          manager: 4,
+        },
+      ],
+    },
+  ],
+};
 
 createServer({
-    routes() {
-        this.namespace = "api"
+  routes() {
+    this.namespace = "api";
 
-        this.get("/employee/graph", () => {
-            return {
-                data: graphData
+    this.get(
+      "/employee/graph",
+      () => {
+        return {
+          data: graphData,
+        };
+      },
+      [graphData]
+    );
+
+    this.post("/employee/graph/edit", (schema, request) => {
+      let attrs = JSON.parse(request.requestBody);
+      if (attrs.dropValue === graphData.id) {
+        let tempData = {};
+        graphData.hChildren.map((items) => {
+          items.children.map((item) => {
+            if (item.id === attrs.dragValue) {
+              item.manager = attrs.dropValue;
+              tempData = item;
+              items.children.splice(
+                items.children.findIndex((a) => a.id === attrs.dragValue),
+                1
+              );
+              return tempData;
             }
-        }, [graphData])
-
-        this.post("/employee/graph/edit", (schema, request) => {
-            let attrs = JSON.parse(request.requestBody)
-            if (attrs.dropValue === graphData.id) {
-                let tempData = {};
-                graphData.hChildren.map((items) => {
-                    items.children.map((item) => {
-                        if (item.id === attrs.dragValue) {
-                            item.manager = attrs.dropValue
-                            tempData = item
-                            items.children.splice(items.children.findIndex(a => a.id === attrs.dragValue), 1)
-                            return tempData
-                        }
-                    })
-
-                })
-                graphData.children.push(tempData)
-            } else {
-                let tempData = {};
-                if (graphData.children.length > 0) {
-                    graphData.children.map((items) => {
-                        if (items.id === attrs.dragValue) {
-                            items.manager = attrs.dropValue
-                            tempData = items
-                            graphData.children.splice( graphData.children.findIndex(a => a.id === attrs.dragValue), 1)
-                            return tempData
-                        }
-                    })
-                }
-                graphData.hChildren.map((items) => {
-
-                    items.children.map((item) => {
-                        if (item.id === attrs.dragValue) {
-                            item.manager = attrs.dropValue
-                            tempData = item
-                            items.children.splice(items.children.findIndex(a => a.id === attrs.dragValue), 1)
-                            return tempData
-                        }
-                    })
-
-
-                })
-                graphData.hChildren.map((value) => {
-                    if (value.id === attrs.dropValue) {
-                        value.children.push(tempData)
-                    }
-                })
+          });
+        });
+        graphData.children.push(tempData);
+      } else {
+        let tempData = {};
+        if (graphData.children.length > 0) {
+          graphData.children.map((items) => {
+            if (items.id === attrs.dragValue) {
+              items.manager = attrs.dropValue;
+              tempData = items;
+              graphData.children.splice(
+                graphData.children.findIndex((a) => a.id === attrs.dragValue),
+                1
+              );
+              return tempData;
             }
-        })
-
-        this.get("/employee", () => {
-            return {
-                data: [
-                    { id: 1, name: "Mark Hill", designation: "Chief executive officer", team: "ceo" },
-                    { id: 2, name: "Joe Linux", designation: "Chief Technology", team: "developer", manager: 1 },
-                    { id: 3, name: "Linda May", designation: "Chief Business Officer", team: "sales", manager: 1 },
-                    { id: 4, name: "John Green", designation: "Chief Accounting officer", team: "accounts", manager: 1 },
-                    { id: 5, name: "Ron Blomquist", designation: "Chief Information", team: "developer", manager: 2 },
-                    { id: 6, name: "Michael Rubin", designation: "Chief Innovation officer", team: "developer", manager: 2 },
-                    { id: 7, name: "Alice Lopex", designation: "Chief Communication officer", team: "sales", manager: 3 },
-                    { id: 8, name: "Mary Johnson", designation: "Chief Brand officer", team: "sales", manager: 3 },
-                    { id: 9, name: "Krik Douglas", designation: "Chief Buisness", team: "sales", manager: 3 },
-                    { id: 10, name: "Erica Reel", designation: "Chief Customer Officer", team: "accounts", manager: 4 },
-                ],
+          });
+        }
+        graphData.hChildren.map((items) => {
+          items.children.map((item) => {
+            if (item.id === attrs.dragValue) {
+              item.manager = attrs.dropValue;
+              tempData = item;
+              items.children.splice(
+                items.children.findIndex((a) => a.id === attrs.dragValue),
+                1
+              );
+              return tempData;
             }
-        })
-    },
-})
+          });
+        });
+        graphData.hChildren.map((value) => {
+          if (value.id === attrs.dropValue) {
+            value.children.push(tempData);
+          }
+        });
+      }
+    });
+
+    this.get("/employee", () => {
+      return {
+        data: [
+          {
+            id: 1,
+            name: "Mark Hill",
+            designation: "Chief executive officer",
+            team: "ceo",
+          },
+          {
+            id: 2,
+            name: "Joe Linux",
+            designation: "Chief Technology",
+            team: "developer",
+            manager: 1,
+          },
+          {
+            id: 3,
+            name: "Linda May",
+            designation: "Chief Business Officer",
+            team: "sales",
+            manager: 1,
+          },
+          {
+            id: 4,
+            name: "John Green",
+            designation: "Chief Accounting officer",
+            team: "accounts",
+            manager: 1,
+          },
+          {
+            id: 5,
+            name: "Ron Blomquist",
+            designation: "Chief Information",
+            team: "developer",
+            manager: 2,
+          },
+          {
+            id: 6,
+            name: "Michael Rubin",
+            designation: "Chief Innovation officer",
+            team: "developer",
+            manager: 2,
+          },
+          {
+            id: 7,
+            name: "Alice Lopex",
+            designation: "Chief Communication officer",
+            team: "sales",
+            manager: 3,
+          },
+          {
+            id: 8,
+            name: "Mary Johnson",
+            designation: "Chief Brand officer",
+            team: "sales",
+            manager: 3,
+          },
+          {
+            id: 9,
+            name: "Krik Douglas",
+            designation: "Chief Buisness",
+            team: "sales",
+            manager: 3,
+          },
+          {
+            id: 10,
+            name: "Erica Reel",
+            designation: "Chief Customer Officer",
+            team: "accounts",
+            manager: 4,
+          },
+        ],
+      };
+    });
+  },
+});
