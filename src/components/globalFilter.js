32f7e9a3d1765ae11e4fffe2c9d12fef02ea5d78
unknown <aswath@accuknox.com>
2022-02-23 23:41:10 +0530

    prettier

--- src/components/globalFilter.js
+++ src/components/globalFilter.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export const GlobalFilter = ({ filter, setFilter }) => {
-    return (
-        <span>
-            <input style={{ width: '140%' }} className='m-2' placeholder='search by Name / designation / team' type="text" value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
-        </span>
-    )
-}
+  return (
+    <span>
+      <input
+        style={{ width: "140%" }}
+        className="m-2"
+        placeholder="search by Name / designation / team"
+        type="text"
+        value={filter || ""}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+    </span>
+  );
+};
